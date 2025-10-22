130a3a3eb0460662dd477bde65f2c6aeef57c9aa
MSami <mosami0606@gmail.com>
2025-10-22 13:47:34 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,28 @@
+const inputBtn = document.querySelector("#input-btn")
+let myLeads = [];
+const inputEl = document.querySelector("#input-el");
+const ulEl = document.querySelector("#ul-el");
+
+
+inputBtn.addEventListener("click", function () {
+    myLeads.push(inputEl.value);
+    renderLeads();
+    inputEl.value='';
+
+})
+
+
+function renderLeads() {
+    let listItems = "";
+    for (let i = 0; i < myLeads.length; i++) {
+        // listItems += "<li>" + "<a target='_blank' href='#'>" + myLeads[i] + "</a>"+ "</li>"
+        listItems += `
+        <li>
+            <a target='_blank' href="${myLeads[i]}">
+                ${myLeads[i]}
+            </a>
+        </li>`
+    }
+    ulEl.innerHTML = listItems;
+
+}
---
+++ practice.js
@@ -0,0 +1,5 @@
+const recipient = "james"
+const sender = 'coder'
+const email = `Hey ${recipient}! How is it going? Cheers ${sender}`;
+
+console.log(email)
