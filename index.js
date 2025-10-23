3c18a504c08a214d920474b4770588f4715a99ab
MSami <mosami0606@gmail.com>
2025-10-23 15:29:12 -0400

    Final version of app

--- index.js
+++ index.js
@@ -2,27 +2,62 @@ const inputBtn = document.querySelector("#input-btn")
 let myLeads = [];
 const inputEl = document.querySelector("#input-el");
 const ulEl = document.querySelector("#ul-el");
+let deleteBtn = document.querySelector("#delete-btn")
+let tabBtn = document.querySelector("#tab-btn")
 
+// myLeads = JSON.parse(myLeads);
+// myLeads.push("example")
+// myLeads = JSON.stringify(myLeads)
+// console.log(typeof myLeads)
+
+const leadsFromLocalStorage = JSON.parse
+    (localStorage.getItem("myLeads"))
+
+
+if (leadsFromLocalStorage) {
+    myLeads = leadsFromLocalStorage
+    render(myLeads);
+}
+
+function render(leads) {
+    let listItems = "";
+    for (let i = 0; i < leads.length; i++) {
+        listItems += `
+        <li>
+            <a target='_blank' href="${leads[i]}">
+                ${leads[i]}
+            </a>
+        </li>`
+    }
+    ulEl.innerHTML = listItems
+
+}
+
+tabBtn.addEventListener("click", function () {
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+        myLeads.push(tabs[0].url)
+        localStorage.setItem("myLeads", JSON.stringify(myLeads))
+        render(myLeads)
+    })
+})
+
+
+deleteBtn.addEventListener("dblclick", function () {
+    localStorage.clear();
+    myLeads = [];
+    render(myLeads);
+
+})
 
 inputBtn.addEventListener("click", function () {
     myLeads.push(inputEl.value);
-    renderLeads();
-    inputEl.value='';
+    inputEl.value = '';
+
+    localStorage.setItem("myLeads", JSON.stringify(myLeads))
+
+    render(myLeads);
+    console.log(localStorage.getItem("myLeads"))
 
 })
 
 
-function renderLeads() {
-    let listItems = "";
-    for (let i = 0; i < myLeads.length; i++) {
-        // listItems += "<li>" + "<a target='_blank' href='#'>" + myLeads[i] + "</a>"+ "</li>"
-        listItems += `
-        <li>
-            <a target='_blank' href="${myLeads[i]}">
-                ${myLeads[i]}
-            </a>
-        </li>`
-    }
-    ulEl.innerHTML = listItems;
-
-}
--- practice.js
+++
@@ -1,5 +0,0 @@
-const recipient = "james"
-const sender = 'coder'
-const email = `Hey ${recipient}! How is it going? Cheers ${sender}`;
-
-console.log(email)
